04b6e64d6a7a003bfc10fa2bc12ae31d91cb567b
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-04-28 12:08:36 +0200

    Create ability to save article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    articles_params = params.require(:article).permit(:title, :text)
+    @article = Article.new(articles_params)
+    @article.save
+
+    redirect_to articles_path
+  end
 end
