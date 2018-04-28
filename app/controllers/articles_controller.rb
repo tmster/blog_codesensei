3d21b41d6f87334c601b7232752b50d8b70a70a5
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-04-28 14:51:45 +0200

    Create ability to edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,4 +21,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    articles_params = params.require(:article).permit(:title, :text)
+    if @article.update(articles_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 end
