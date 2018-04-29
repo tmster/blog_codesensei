bb2d47c30f6248c005db0b2448e2facd23b95640
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-04-29 12:02:13 +0200

    Move comments under articles in router

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @article = Article.find(params[:comment][:article_id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.build(comment_params)
     @comment.save
     redirect_to article_path(@article)
