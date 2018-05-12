fdcbbfba06d6e0c97c4ed352b3c695022024e72d
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-05-12 11:45:28 +0200

    Add tags to views in article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :tags)
   end
 
   def find_article
