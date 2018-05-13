23d513fd4355d787146bc7cc377201f36b65f0dc
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-05-13 14:34:58 +0200

    Fix bug with missing variable @like

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(comment_params)
+    @like = Like.find_or_initialize_by(article: @article, user: current_user)
 
     if @comment.save
       session[:commenter] = @comment.commenter
