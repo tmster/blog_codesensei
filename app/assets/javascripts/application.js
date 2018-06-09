a9a349a5922e3ede508cd2ecdd539d616e1d69e9
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2018-06-09 11:48:16 +0200

    Create ability to toggle comments view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,21 @@
 //
 //= require rails-ujs
 //= require_tree .
+
+window.addEventListener("load", function() {
+  var toggleButton = document.getElementById("toggle-comments")
+  if (toggleButton != null) {
+    toggleButton.addEventListener("click", function(e) {
+      e.preventDefault();
+      var comments = document.getElementById("comments");
+      if(comments.style.display == "none") {
+        comments.style.display = "block";
+        this.innerHTML = "HIDE comments";
+      } else {
+        comments.style.display = "none";
+        this.innerHTML = "SHOW comments";
+      }
+    });
+  }
+
+});
